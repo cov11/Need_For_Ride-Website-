4475129207f7c54360e8d582beaee79661944a10
maissa <maisayousfi095@gmail.com>
2025-04-14 14:33:26 +0100

    ajout de footer.php

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <div style="text-align: center; padding: 20px; background-color:green; color: white;">
+        <p>&copy; 2025 Need For Ride. Tous droits réservés.</p>
+    </div>
+</footer>
+
+</body>
+</html>
